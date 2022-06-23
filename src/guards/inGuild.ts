72a13b41b01469abe5565b0b01d33900484eec7d
AtoraSuunva <atora@giraffeduck.com>
2022-06-23 14:02:17 -0400

    fix(guards): inGuild now resolves to the appropriate CacheType instead of always cached

--- src/guards/inGuild.ts
+++ src/guards/inGuild.ts
@@ -12,40 +12,42 @@ import {
 } from 'discord.js'
 import { PreRunError } from '../errors/PreRunError.js'
 
+type CacheType = 'cached' | 'raw'
+
 export function inGuild(
   interaction: ButtonInteraction,
-): asserts interaction is ButtonInteraction<'cached'>
+): asserts interaction is ButtonInteraction<CacheType>
 export function inGuild(
   interaction: SelectMenuInteraction,
-): asserts interaction is SelectMenuInteraction<'cached'>
+): asserts interaction is SelectMenuInteraction<CacheType>
 export function inGuild(
   interaction: MessageComponentInteraction,
-): asserts interaction is MessageComponentInteraction<'cached'>
+): asserts interaction is MessageComponentInteraction<CacheType>
 
 export function inGuild(
   interaction: MessageContextMenuInteraction,
-): asserts interaction is MessageContextMenuInteraction<'cached'>
+): asserts interaction is MessageContextMenuInteraction<CacheType>
 export function inGuild(
   interaction: UserContextMenuInteraction,
-): asserts interaction is UserContextMenuInteraction<'cached'>
+): asserts interaction is UserContextMenuInteraction<CacheType>
 export function inGuild(
   interaction: ContextMenuInteraction,
-): asserts interaction is ContextMenuInteraction<'cached'>
+): asserts interaction is ContextMenuInteraction<CacheType>
 
 export function inGuild(
   interaction: CommandInteraction,
-): asserts interaction is CommandInteraction<'cached'>
+): asserts interaction is CommandInteraction<CacheType>
 export function inGuild(
   interaction: BaseCommandInteraction,
-): asserts interaction is BaseCommandInteraction<'cached'>
+): asserts interaction is BaseCommandInteraction<CacheType>
 
 export function inGuild(
   interaction: AutocompleteInteraction,
-): asserts interaction is AutocompleteInteraction<'cached'>
+): asserts interaction is AutocompleteInteraction<CacheType>
 
 export function inGuild(
   interaction: Interaction,
-): asserts interaction is Interaction<'cached'> {
+): asserts interaction is Interaction<CacheType> {
   if (!interaction.inGuild()) {
     throw new PreRunError('This command must be run in a guild.')
   }
