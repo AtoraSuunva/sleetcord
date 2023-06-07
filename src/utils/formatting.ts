40ba4a48cf34db8a3d82778b46f3e0fa7cde57b4
AtoraSuunva <atora@giraffeduck.com>
2023-06-06 22:55:23 -0400

    feat(format): partial support for new usernames, waiting on d.js to finish support

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -9,16 +9,24 @@ export interface FormatUserOptions {
   mention?: boolean
   /** Insert a left-to-right mark after the username */
   bidirectional?: boolean
+  /** Show the user's global name + username */
+  globalName?: boolean
 }
 
 /**
  * Formats a user in following way:
  *
+ * Old tag usernames:
  *   `**Username**#1234 (id)`
+ *
+ * New usernames:
+ *   `Global Name **username** (id)`
+ *
+ * TODO: Actually add Global Name when it's in d.js (see PR https://github.com/discordjs/discord.js/pull/9512)
  *
  * A Left-to-Right mark is inserted after the username to prevent RTL usernames from messing up the rest of the string
  * @param userLike A User or GuildMember to format
- * @param param1 How to format the user
+ * @param params How to format the user
  * @returns A formatted string for the user
  */
 export function formatUser(
@@ -38,7 +46,7 @@ export function formatUser(
   formatted.push(escapeMarkdown(user.username))
   if (markdown) formatted.push('**')
   if (bidirectional) formatted.push('\u{200e}')
-  formatted.push(`#${user.discriminator}`)
+  if (user.discriminator !== '0') formatted.push(`#${user.discriminator}`)
   if (id) formatted.push(` (${user.id})`)
   if (mention) formatted.push(` <@${user.id}>`)
 
