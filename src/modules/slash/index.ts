71fb988a1f7cbfc0f0981bfe376e0d035c9a3cde
AtoraSuunva <atora@giraffeduck.com>
2023-09-20 18:18:28 -0400

    release(1.2.0): more exports

--- src/modules/slash/index.ts
+++ src/modules/slash/index.ts
@@ -1,6 +1,8 @@
+export * from './SleetAutocompleteable.js'
 export * from './SleetSlashCommand.js'
 export * from './SleetSlashCommandGroup.js'
 export * from './SleetSlashSubcommand.js'
+export type * from './SleetAutocompleteable.js'
 export type * from './SleetSlashCommand.js'
 export type * from './SleetSlashCommandGroup.js'
 export type * from './SleetSlashSubcommand.js'
